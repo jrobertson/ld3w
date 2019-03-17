9c50f4b658f2c26ce73afab1a56ab75ef2a7cdce
James Robertson <james@jessie.qbitx.com>
2019-03-17 19:50:28 +0000

    feature: Verbose mode can now be set to false from the initialize keyword *verbose*.

--- lib/ld3w.rb
+++ lib/ld3w.rb
@@ -12,19 +12,18 @@ end
 class Ld3w
 
 
-  def initialize(device: '', bdaddress: 'rfcomm0', channel: '1', affirmations: 5)
+  def initialize(device: '', bdaddress: 'rfcomm0', channel: '1', 
+                 affirmations: 5, verbose: true)
 
     @command = "rfcomm connect %s %s %s" % [device, bdaddress, channel]
     @file = '/dev/' + device
     @np = NMEAParser.new
-    @affirmations = affirmations
-    @device = device
+    @affirmations, @device, @verbose = affirmations, device, verbose
 
   end
   
   def locate()
 
-    line = ''
     lines = []
     
     IO.popen @command, autoclose: true do |io|
@@ -35,25 +34,13 @@ class Ld3w
       (sleep 1; puts 'waiting ...') until File.exists?(@file) or 
           Time.now > (t + 10)
       
-      raise(Ld3wError, 'Cannot open file ' + @file) unless File.exists? @file
-
-      # get the data from the file
-
-      pattern = /^\$GPGGA/
-      line_count = 0
-
-      IO.foreach(@file) do |x| 
-
-        print "GOT ", x
-        c = x.split(',').count('')
-
-        if x =~ pattern and c <= 2 then
-          lines << x 
-          line_count > @affirmations ? break : line_count += 1
-        end
-        sleep 0.05
+      unless File.exists? @file then
+        
+        `rfcomm release #{file}`
+        raise(Ld3wError, 'Cannot open file ' + @file) 
       end
-      sleep 2
+
+      lines = read_file()
 
       puts 'bye'
       Process.kill('INT', io.pid)
@@ -62,9 +49,32 @@ class Ld3w
     
     #`rfcomm release #{@device}`
 
-    lines.map {|x| @np.parse(x) }
+    lines
 
 
+  end
+  
+  def read_file()
+    
+    lines = []
+    pattern = /^\$GPGGA/
+    line_count = 0
+
+    IO.foreach(@file) do |x| 
+
+      print "GOT ", x if @verbose
+      c = x.split(',').count('')
+
+      if x =~ pattern and c <= 2 then
+        lines << x 
+        line_count > @affirmations ? break : line_count += 1
+      end
+      sleep 0.05
+    end
+    sleep 2
+    
+
+    lines.map {|x| @np.parse(x) }    
   end
 
 end
